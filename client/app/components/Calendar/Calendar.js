ac0e3228b2322f817fff434af75c93a48b1255eb
Mouatassim <motalei4991@gmail.com>
2018-11-20 23:30:28 +0100

    calander edit

--- client/app/components/Calendar/Calendar.js
+++ client/app/components/Calendar/Calendar.js
@@ -1,49 +1,115 @@
-import React from "react";
+import React, { Component } from "react";
+
 import  Title from './Title'
 
-class Home extends React.Component{
+class Calendar extends Component{
     constructor (){
         super();
         this.state={
-            'users': [],
-            'greeting' : "Hello dude",
-            'language' : "Engilish"
+            
+            'tasks' : [
+                {'id':1, 'name':"eat", 'des':"put food in stomach"},{'id':2, 'name':"run", 'des':"walk fast"},{'id':3, 'name':"devlope", 'des':"build apps"}
+            ],
+            'language' : "Engilish",
+            'counter' : 0,
         };
       }
 
-       frenchIt() {
-         if(this.state.greeting == "Hello dude"){
-        this.setState({greeting:"bonjour mon frere"})
-         }else{
-          this.setState({greeting:"Hello dude"})
-         }
+
+      handelSubmit(newTask){
+        var NewTasks = this.state.tasks;
+        this.setState({
+            tasks: NewTasks.push(newTask)
+        })
       }
-      
-      changeLanguage(language){
-        this.setState({language})
+
+      inrimentCounter(){
+        var pre = this.state.counter +1
+        this.setState({
+            counter: pre
+               
+        })
       }
-    
+   
     render(){
 
-        return(
+        return (
+            <div>
+                
+            <App tasks={this.state.tasks} handelSubmit={this.handelSubmit.bind(this)} />
+            
+            <h3>{this.state.counter}</h3>
+            <button onClick={this.inrimentCounter.bind(this)}>+</button>
 
-          <div>
-              <Title changeLanguage ={this.changeLanguage.bind(this)} ref={this.state.greeting} language={this.state.language} />
-               <div>
-                   <h1>{this.state.greeting}</h1>
-              </div>
+            </div>
               
-                                      
-              <input type="button"  onClick={this.frenchIt.bind(this)}/>
-
-              <div>
-
-                  <p>A 3ataay</p>
-              </div>    
-          </div>    
-          
         )
+        
       }
     }
+ 
+    class App extends Component{
+        constructor (props){
+            super(props);
+            this.state={
+               
+                'name'  : "",
+                'des'   :"",
+                'tasks' : this.props.tasks
+            };
+          }
     
-    export default Home;
+           
+        _handelName(event){
+            this.setState({
+             name : event.target.value
+            })
+        }
+        _handelDescrip(event){
+            this.setState({
+             des : event.target.value
+            })
+        }
+        addTask(){
+            const newTask = {
+                id: Date.now(),
+                name: this.state.name,
+                des : this.state.des
+
+            };
+            this.props.handelSubmit(newTask);
+            console.log(this.state.tasks);
+            this.setState({
+                name : "",
+                des : "",
+            })
+            
+        }
+        render(){
+    
+           return(
+         <div>
+                         <ul >
+            {this.state.tasks.map((task)=>{
+               
+                
+               return <li key={task.id}> {task.name} : {task.des}</li>
+            })}    
+            </ul>
+              <h1>
+                 {this.state.name}
+              </h1>
+                <input placeholder="Name" value={this.state.name} onChange={this._handelName.bind(this)}/>
+                <input placeholder="Description" value={this.state.des} onChange={this._handelDescrip.bind(this)}/>
+                <button onClick={this.addTask.bind(this)}>Add</button>
+          </div> 
+           )
+    
+                  
+            
+            
+          }
+        }
+     
+
+    export default Calendar;
